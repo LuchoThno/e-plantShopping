d6cf4cbb7defdfdc0dfd7c84472c6f1dc26211ca
LuchoThno <luisevargasve@gmail.com>
2025-10-25 21:58:28 -0300

    Clear branch for GitHub Pages

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/shopping_learning_react",
-  plugins: [react()],
-})
